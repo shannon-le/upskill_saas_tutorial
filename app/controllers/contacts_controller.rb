d84161ff35bfbe6e3f77c8aeba24c18ff55a3b28
Shannon Le <shannon_le@outlook.com>
2020-08-13 20:17:27 +0000

    Updated contact url

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
             flash[:success] = "Message sent."
             redirect_to new_contact_path
         else
-            flash[:danger] = @contact.errors.full_messages.join(", ")
+            flash[:destroy] = @contact.errors.full_messages.join(", ")
             redirect_to new_contact_path
         end
     end
